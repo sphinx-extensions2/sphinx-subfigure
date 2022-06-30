af8ba1a814ca5d1014cd8849b8ff33b368428bef
Chris Sewell <chrisj_sewell@hotmail.com>
2022-06-30 22:44:40 +0200

    👌 IMPROVE: Centralise CSS class -> grid-template-areas

--- src/sphinx_subfigure/main.py
+++ src/sphinx_subfigure/main.py
@@ -6,22 +6,15 @@ import string
 from docutils import nodes
 from docutils.parsers.rst import directives
 from sphinx.application import Sphinx
-from sphinx.transforms.post_transforms import SphinxPostTransform
 from sphinx.util.docutils import SphinxDirective
-from sphinx.writers.html import HTMLTranslator
+
+from .tr_html import setup_html
 
 
 def setup(app: Sphinx) -> None:
     """Setup the extension."""
     app.add_directive("subfigure", SubfigureDirective)
-    app.add_post_transform(SubfigureHtmlTransform)
-    app.add_node(
-        SubfigureGridHtml, html=(visit_subfigure_grid_html, depart_subfigure_grid_html)
-    )
-    app.add_node(
-        SubfigureGridItemHtml,
-        html=(visit_subfigure_grid_item_html, depart_subfigure_grid_item_html),
-    )
+    setup_html(app)
 
 
 class SubfigureDirective(SphinxDirective):
@@ -33,6 +26,7 @@ class SubfigureDirective(SphinxDirective):
     final_argument_whitespace = True
     option_spec = {
         "layout-sm": directives.unchanged,
+        "layout-lg": directives.unchanged,
         "subcaptions": lambda x: directives.choice(x, ("above", "below")),
         "width": directives.length_or_percentage_or_unitless,
         "align": lambda x: directives.choice(x, ("left", "center", "right")),
@@ -77,11 +71,17 @@ class SubfigureDirective(SphinxDirective):
                 )
 
         layout_string = self.arguments[0] if self.arguments else 1
-        figure_node["layout"] = self.generate_layout(layout_string, number_of_images)
-
+        figure_node["layout"] = {}
+        figure_node["layout"]["default"] = self.generate_layout(
+            layout_string, number_of_images
+        )
         if "layout-sm" in self.options:
-            figure_node["layout_sm"] = self.generate_layout(
+            figure_node["layout"]["sm"] = self.generate_layout(
                 self.options.get("layout-sm") or 1, number_of_images, "layout-sm"
+            )
+        if "layout-lg" in self.options:
+            figure_node["layout"]["lg"] = self.generate_layout(
+                self.options.get("layout-lg") or 1, number_of_images, "layout-lg"
             )
 
         return [figure_node]
@@ -177,123 +177,3 @@ class SubfigureDirective(SphinxDirective):
                 row.append(col)
             layout.append(row)
         return layout
-
-
-class SubfigureGridHtml(nodes.General, nodes.Element):
-    """Node for subfigure grid, added only for Html."""
-
-
-def visit_subfigure_grid_html(self: HTMLTranslator, node: SubfigureGridHtml) -> None:
-    """Visit subfigure grid node."""
-    classes = " ".join(node["classes"])
-    layout = " ".join(["'" + " ".join(row) + "'" for row in node["layout"]])
-    if node.get("layout_sm"):
-        layout_default_class = "layout-default-" + "_".join(
-            ["".join(row) for row in node["layout"]]
-        )
-        layout_sm = " ".join(["'" + " ".join(row) + "'" for row in node["layout_sm"]])
-        layout_sm_class = "layout-sm-" + "_".join(
-            ["".join(row) for row in node["layout_sm"]]
-        )
-        classes += " " + layout_default_class + " " + layout_sm_class
-        # TODO deduplicate-this? make width configurable?
-        self.body.append(
-            "<style>\n"
-            f"div.{layout_default_class} {{\n"
-            f"  grid-template-areas: {layout};\n"
-            "}\n"
-            "@media (max-width: 576px) {\n"
-            f"  div.{layout_sm_class} {{\n"
-            f"    grid-template-areas: {layout_sm};\n"
-            "  }\n"
-            "}\n"
-            "</style>\n"
-        )
-        self.body.append(f'<div class="{classes}" style="display: grid;">\n')
-    else:
-        self.body.append(
-            f'<div class="{classes}" style="display: grid; grid-template-areas: {layout};">\n'
-        )
-
-
-def depart_subfigure_grid_html(self: HTMLTranslator, node: SubfigureGridHtml) -> None:
-    """Depart subfigure grid node."""
-    self.body.append("</div>\n")
-
-
-class SubfigureGridItemHtml(nodes.General, nodes.Element):
-    """Node for subfigure grid item, added only for Html."""
-
-
-def visit_subfigure_grid_item_html(
-    self: HTMLTranslator, node: SubfigureGridItemHtml
-) -> None:
-    """Visit subfigure grid item node."""
-    classes = " ".join(node["classes"])
-    style = "display: flex; flex-direction: column; justify-content: center; align-items: center;"
-    style += f" grid-area: {node['area']};"
-    self.body.append(f'<div class="{classes}" style="{style}">\n')
-    if "caption" in node and node["caption-align"] == "above":
-        self.body.append(f'<span class="caption">{node["caption"]}</span>\n')
-
-
-def depart_subfigure_grid_item_html(
-    self: HTMLTranslator, node: SubfigureGridItemHtml
-) -> None:
-    """Depart subfigure grid item node."""
-    if "caption" in node and node["caption-align"] == "below":
-        self.body.append(f'<span class="caption">{node["caption"]}</span>\n')
-    self.body.append("</div>\n")
-
-
-class SubfigureHtmlTransform(SphinxPostTransform):
-    """Transform subfigure containers into the HTML specific AST structures."""
-
-    default_priority = 199
-    formats = ("html",)
-
-    def run(self) -> None:
-        """Run the transform."""
-
-        # docutils <0.18 (traverse) >=0.18 (findall) compatibility
-        for fig_node in _findall(
-            self.document, lambda n: "is_subfigure" in getattr(n, "attributes", {})
-        ):
-
-            # initiate figure children
-            children = []
-
-            # add grid
-            grid_node = SubfigureGridHtml(
-                layout=fig_node["layout"], classes=fig_node.get("grid_classes", [])
-            )
-            if "layout_sm" in fig_node:
-                grid_node["layout_sm"] = fig_node["layout_sm"]
-            children.append(grid_node)
-
-            # add image items to grid
-            caption = None
-            item_classes = ["sphinx-subfigure-item"] + fig_node.get("item_classes", [])
-            for child in fig_node:
-                if isinstance(child, nodes.caption):
-                    caption = child
-                    continue
-                item_node = SubfigureGridItemHtml(classes=item_classes)
-                # if fig_node["layout_type"] == "areas":
-                item_node["area"] = child["subfigure_area"]
-                if fig_node["subcaptions"] and child.get("alt"):
-                    item_node["caption-align"] = fig_node["subcaptions"]
-                    item_node["caption"] = child["alt"]
-                item_node.append(child)
-                grid_node.append(item_node)
-
-            # add caption
-            if caption:
-                children.append(caption)
-
-            fig_node.children = children
-
-
-def _findall(node, *args, **kwargs):
-    """Compatibility for docutils <0.18."""
-    return getattr(node, "findall", node.traverse)(*args, **kwargs)
