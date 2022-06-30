db7a44bc120e52c9d818df6badf241c135c3206c
Chris Sewell <chrisj_sewell@hotmail.com>
2022-06-30 22:58:51 +0200

    📚 DOCS: Fix RTD build

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,16 @@
+from sphinx_subfigure import __version__
+
 project = "Sphinx Subfigures"
 author = "Chris Sewell"
+copyright = "2022, Chris Sewell"
+version = __version__
 
 extensions = ["myst_parser", "sphinx_subfigure"]
 
 myst_enable_extensions = ["colon_fence"]
 numfig = True
+
+suppress_warnings = ["epub.unknown_project_files"]
 
 html_theme = "furo"
 html_static_path = ["_static"]
